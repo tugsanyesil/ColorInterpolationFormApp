da5c42874cfd1359692066c4433e098272d85d04
Tuğsan Yeşil <tugsanyesil@gmail.com>
2023-06-29 19:31:03 +0300

    Add project files.

---
+++ ColorInterpolationFormApp/ColorInterpolationForm.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace ColorInterpolationFormApp
+{
+    partial class ColorInterpolationForm
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.Bar = new System.Windows.Forms.TrackBar();
+            this.CountLabel = new System.Windows.Forms.Label();
+            this.ResultPanel = new System.Windows.Forms.Panel();
+            this.Graph = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.ResultLabel = new System.Windows.Forms.Label();
+            this.AddDataButton = new System.Windows.Forms.Button();
+            this.DataLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Bar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Graph)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Bar
+            // 
+            this.Bar.Location = new System.Drawing.Point(12, 12);
+            this.Bar.Maximum = 100;
+            this.Bar.Name = "Bar";
+            this.Bar.Size = new System.Drawing.Size(291, 45);
+            this.Bar.TabIndex = 0;
+            this.Bar.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.Bar.Scroll += new System.EventHandler(this.Bar_Scroll);
+            // 
+            // CountLabel
+            // 
+            this.CountLabel.AutoSize = true;
+            this.CountLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.CountLabel.Location = new System.Drawing.Point(309, 12);
+            this.CountLabel.Name = "CountLabel";
+            this.CountLabel.Size = new System.Drawing.Size(87, 31);
+            this.CountLabel.TabIndex = 1;
+            this.CountLabel.Text = "Count";
+            // 
+            // ResultPanel
+            // 
+            this.ResultPanel.Location = new System.Drawing.Point(12, 63);
+            this.ResultPanel.Name = "ResultPanel";
+            this.ResultPanel.Size = new System.Drawing.Size(200, 200);
+            this.ResultPanel.TabIndex = 2;
+            // 
+            // Graph
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.Graph.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.Graph.Legends.Add(legend1);
+            this.Graph.Location = new System.Drawing.Point(393, 43);
+            this.Graph.Name = "Graph";
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series1.Color = System.Drawing.Color.Black;
+            series1.Legend = "Legend1";
+            series1.Name = "Appereance";
+            series2.BorderWidth = 3;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series2.Color = System.Drawing.Color.Red;
+            series2.Legend = "Legend1";
+            series2.Name = "Red";
+            series3.BorderWidth = 3;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series3.Color = System.Drawing.Color.Green;
+            series3.Legend = "Legend1";
+            series3.Name = "Green";
+            series4.BorderWidth = 3;
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series4.Color = System.Drawing.Color.Blue;
+            series4.Legend = "Legend1";
+            series4.Name = "Blue";
+            this.Graph.Series.Add(series1);
+            this.Graph.Series.Add(series2);
+            this.Graph.Series.Add(series3);
+            this.Graph.Series.Add(series4);
+            this.Graph.Size = new System.Drawing.Size(770, 228);
+            this.Graph.TabIndex = 3;
+            this.Graph.Text = "chart1";
+            // 
+            // ResultLabel
+            // 
+            this.ResultLabel.AutoSize = true;
+            this.ResultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ResultLabel.Location = new System.Drawing.Point(218, 146);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(92, 31);
+            this.ResultLabel.TabIndex = 4;
+            this.ResultLabel.Text = "Result";
+            // 
+            // AddDataButton
+            // 
+            this.AddDataButton.BackgroundImage = ColorInterpolationFormApp.Properties.Resources.plus_button_icon;
+            this.AddDataButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.AddDataButton.Location = new System.Drawing.Point(12, 339);
+            this.AddDataButton.Name = "AddDataButton";
+            this.AddDataButton.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.AddDataButton.Size = new System.Drawing.Size(100, 100);
+            this.AddDataButton.TabIndex = 5;
+            this.AddDataButton.UseVisualStyleBackColor = true;
+            this.AddDataButton.Click += new System.EventHandler(this.AddDataButton_Click);
+            // 
+            // DataLabel
+            // 
+            this.DataLabel.AutoSize = true;
+            this.DataLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.DataLabel.Location = new System.Drawing.Point(6, 305);
+            this.DataLabel.Name = "DataLabel";
+            this.DataLabel.Size = new System.Drawing.Size(72, 31);
+            this.DataLabel.TabIndex = 6;
+            this.DataLabel.Text = "Data";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(750, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 31);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Graph";
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(1088, 313);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveButton.TabIndex = 8;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Location = new System.Drawing.Point(1088, 277);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(75, 23);
+            this.LoadButton.TabIndex = 9;
+            this.LoadButton.Text = "Load";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            this.LoadButton.Click += new System.EventHandler(this.LoadButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(938, 274);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 62);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Load/Save\r\nSample";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // ColorInterpolationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1184, 561);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LoadButton);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.DataLabel);
+            this.Controls.Add(this.AddDataButton);
+            this.Controls.Add(this.ResultLabel);
+            this.Controls.Add(this.Graph);
+            this.Controls.Add(this.ResultPanel);
+            this.Controls.Add(this.CountLabel);
+            this.Controls.Add(this.Bar);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.Name = "ColorInterpolationForm";
+            this.Text = "ColorInterpolationForm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ColorInterpolationForm_FormClosing);
+            this.Load += new System.EventHandler(this.ColorInterpolationForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Bar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Graph)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar Bar;
+        private System.Windows.Forms.Label CountLabel;
+        private System.Windows.Forms.Panel ResultPanel;
+        private System.Windows.Forms.DataVisualization.Charting.Chart Graph;
+        private System.Windows.Forms.Label ResultLabel;
+        private System.Windows.Forms.Button AddDataButton;
+        private System.Windows.Forms.Label DataLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Button LoadButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ColorInterpolationFormApp/ColorInterpolationForm.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+using ColorInterpolationLib;
+
+namespace ColorInterpolationFormApp
+{
+    public partial class ColorInterpolationForm : Form
+    {
+        public ColorInterpolationForm()
+        {
+            InitializeComponent();
+        }
+
+        public static string FileFilterExtension = "txt";
+        public static string FileExtension = $".{FileFilterExtension}";
+        public static string FileName = $"data{FileExtension}";
+        public static string FileFilter = $"{FileFilterExtension} files (*{FileExtension})|*{FileExtension}";
+        public ColorInterpolation ColorInt;
+        public List<Button> Buttons = new List<Button>();
+        public Point GridZeroLocation;
+        public Size GridElementOffset;
+        public Size GridElementSize;
+        public Size GridElementSizeWithOffset;
+        public int GridColumn = 9;
+        private void ColorInterpolationForm_Load(object sender, EventArgs e)
+        {
+            GridZeroLocation = AddDataButton.Location;
+            GridElementSize = new Size(100, 100);
+            GridElementOffset = new Size(10, 10);
+            GridElementSizeWithOffset = AddDataButton.Size + GridElementOffset;
+
+            AddDataButton.Size = GridElementSize;
+
+            Form_Refresh(true, "");
+        }
+
+        private void Form_Refresh(bool bool_load, string FileName)
+        {
+            if (FileName != null)
+            {
+                if (bool_load)
+                {
+                    if (FileName == "")
+                    {
+                        FileName = ColorInterpolationForm.FileName;
+                    }
+                    ColorInt = ColorInterpolation.Load(Path.Combine(GetExecutingDirectoryName(), FileName));
+                }
+
+                Buttons.ForEach(button => Controls.Remove(button));
+                AddDataButton.Location = GridZeroLocation;
+                Buttons.Clear();
+
+                for (int i = 0; i < ColorInt.Doubles.Length; i++)
+                {
+                    AddData(ColorInt.Colors[i], ColorInt.Doubles[i]);
+                }
+
+                Bar.Minimum = (int)(Math.Ceiling(ColorInt.Doubles.ToList().Min()));
+                Bar.Maximum = (int)(Math.Floor(ColorInt.Doubles.ToList().Max()));
+                Bar.TickFrequency = 10 * Buttons.Count;
+            }
+
+            Bar_Scroll(null, EventArgs.Empty);
+
+            double number_of_points = 10;
+
+            Graph.Series[0].Points.Clear();
+            Graph.Series[1].Points.Clear();
+            Graph.Series[2].Points.Clear();
+            Graph.Series[3].Points.Clear();
+
+            double difference = Bar.Maximum - Bar.Minimum;
+            if (difference > 0)
+            {
+                for (double i = Bar.Minimum; i <= Bar.Maximum; i += difference / number_of_points)
+                {
+                    var color = ColorInt.DoubleToColor(i).Value;
+
+                    Graph.Series[0].Points.AddXY(i, color.A);
+                    Graph.Series[1].Points.AddXY(i, color.R);
+                    Graph.Series[2].Points.AddXY(i, color.G);
+                    Graph.Series[3].Points.AddXY(i, color.B);
+                }
+            }
+        }
+
+        private void Bar_Scroll(object sender, EventArgs e)
+        {
+            CountLabel.Text = Bar.Value.ToString();
+            ResultPanel.BackColor = ColorInt.DoubleToColor(Bar.Value).Value;
+        }
+
+        private static DialogResult InputBox(string title, string promptText, out double value)
+        {
+            Button buttonOk = new Button { Text = "OK", DialogResult = DialogResult.OK, Anchor = AnchorStyles.Bottom | AnchorStyles.Right };
+            Button buttonCancel = new Button { Text = "Cancel", DialogResult = DialogResult.Cancel, Anchor = AnchorStyles.Bottom | AnchorStyles.Right };
+
+            Form form = new Form
+            {
+                Text = title,
+                ClientSize = new Size(396, 107),
+                FormBorderStyle = FormBorderStyle.FixedDialog,
+                StartPosition = FormStartPosition.CenterScreen,
+                MinimizeBox = false,
+                MaximizeBox = false,
+                AcceptButton = buttonOk,
+                CancelButton = buttonCancel
+            };
+
+            Label label = new Label
+            {
+                Text = promptText,
+                AutoSize = true
+            };
+            label.SetBounds(9, 20, 372, 13);
+
+            TextBox textBox = new TextBox { Text = 0.ToString() };
+            textBox.Anchor |= AnchorStyles.Right;
+
+            textBox.SetBounds(12, 36, 372, 20);
+            buttonOk.SetBounds(228, 72, 75, 23);
+            buttonCancel.SetBounds(309, 72, 75, 23);
+
+            form.Controls.AddRange(new Control[] { label, textBox, buttonOk, buttonCancel });
+
+            form.ClientSize = new Size(Math.Max(300, label.Right + 10), form.ClientSize.Height);
+
+            DialogResult dialogResult = form.ShowDialog();
+            if (dialogResult == DialogResult.OK && double.TryParse(textBox.Text, out value))
+            {
+                dialogResult = DialogResult.OK;
+            }
+            else
+            {
+                value = 0;
+                dialogResult = DialogResult.Cancel;
+            }
+
+            return dialogResult;
+        }
+
+        private void GetGridNumbers(int Count, out int gridRow, out int gridColumn)
+        {
+            gridRow = Count / GridColumn;
+            gridColumn = Count - gridRow * GridColumn;
+        }
+
+        private Point GetGridLocation(int Count)
+        {
+            GetGridNumbers(Count, out int gridRow, out int gridColumn);
+            return new Point(GridZeroLocation.X + gridColumn * GridElementSizeWithOffset.Width, GridZeroLocation.Y + gridRow * GridElementSizeWithOffset.Height);
+        }
+
+        private void Item_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            int i = Buttons.FindIndex(button => button == (Button)sender);
+            if (i != -1)
+            {
+                bool refresh = false;
+                if (e.KeyCode == Keys.Right || e.KeyCode == Keys.Left)
+                {
+                    if (!(i == 0 && e.KeyCode == Keys.Left) && !(i == Buttons.Count - 1 && e.KeyCode == Keys.Right))
+                    {
+                        int next_i = i + ((e.KeyCode == Keys.Right) ? 1 : -1);
+
+                        (Buttons[next_i].BackColor, Buttons[i].BackColor) = (Buttons[i].BackColor, Buttons[next_i].BackColor);
+                        (Buttons[next_i].ForeColor, Buttons[i].ForeColor) = (Buttons[i].ForeColor, Buttons[next_i].ForeColor);
+                        (ColorInt.Colors[next_i], ColorInt.Colors[i]) = (ColorInt.Colors[i], ColorInt.Colors[next_i]);
+                        refresh = true;
+                    }
+                }
+                else if (e.KeyCode == Keys.Delete)
+                {
+                    Controls.Remove(Buttons[i]);
+                    Buttons.RemoveAt(i);
+                    ColorInt = new ColorInterpolation(Buttons.Select(button => button.BackColor).ToArray(), Buttons.Select(button => double.Parse(button.Text)).ToArray());
+
+                    for (int j = i; j < Buttons.Count; j++)
+                    {
+                        Buttons[j].Location = GetGridLocation(j);
+                    }
+                    AddDataButton.Location = GetGridLocation(Buttons.Count);
+                    refresh = true;
+                }
+
+                if (refresh)
+                { 
+                    Form_Refresh(false, null); 
+                }
+            }
+        }
+
+        private void AddData(Color color, double value)
+        {
+            Button Item = new Button
+            {
+                Size = GridElementSize,
+                BackColor = color,
+                ForeColor = ((color.R + color.G + color.B) / 3.0) < 128 ? Color.White : Color.Black,
+                Location = GetGridLocation(Buttons.Count),
+                Text = value.ToString()
+            };
+            Item.PreviewKeyDown += Item_PreviewKeyDown;
+            Item.Font = new Font(Item.Font.FontFamily, 25);
+            Buttons.Add(Item);
+            Controls.Add(Item);
+            Item.Show();
+
+            AddDataButton.Location = GetGridLocation(Buttons.Count);
+        }
+
+        private void AddDataButton_Click(object sender, EventArgs e)
+        {
+            ColorDialog dialog = new ColorDialog();
+
+            if (dialog.ShowDialog() == DialogResult.OK && InputBox("InputBox", "Enter a Number", out double value) == DialogResult.OK)
+            {
+                List<Color> colors = ColorInt.Colors.ToList();
+                colors.Add(dialog.Color);
+
+                List<double> doubles = ColorInt.Doubles.ToList();
+                doubles.Add(value);
+
+                ColorInt = new ColorInterpolation(colors.ToArray(), doubles.ToArray());
+            }
+            Form_Refresh(false, "");
+        }
+
+        public static string GetExecutingDirectoryName() => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog() { InitialDirectory = GetExecutingDirectoryName(), Filter = FileFilter };
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                ColorInt.Save(Path.GetFileNameWithoutExtension(dialog.FileName) + ".txt");
+            }
+        }
+
+        private void LoadButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog() { Filter = FileFilter };
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                Form_Refresh(false, dialog.FileName);
+            }
+        }
+
+        private void ColorInterpolationForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            ColorInt.Save(Path.Combine(GetExecutingDirectoryName(), FileName));
+        }
+    }
+}
